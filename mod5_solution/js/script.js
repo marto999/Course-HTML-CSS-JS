5a8adb8c2cac44743909567d59fd652666de8102
Martin <martin.escobar@udla.edu.ec>
2020-08-11 14:03:38 -0500

    Assignment 5

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -83,7 +83,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
             allCategoriesUrl,
-            buildAndShowCategoriesHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+            buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
             true); // Explicitly setting the flag to get JSON from server processed into an object literal
     });
     // *** finish **
@@ -121,12 +121,12 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
-
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
                 // of how to do that.
                 // ....
                 insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
+
             },
             false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
     }
